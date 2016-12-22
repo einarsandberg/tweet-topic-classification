15614de2d716d9a9be4daf7da3477062ad639785
einarsandberg <einars92@gmail.com>
2016-12-22 12:48:04 +0100

    added preprocess.py, used to fetch english tweets from timeline

---
+++ preprocess.py
@@ -0,0 +1,32 @@
+from nltk import wordpunct_tokenize
+import tweepy
+import json
+import csv
+from nltk.corpus import stopwords
+
+
+def fetch_tweets(api, csv_writer):
+    file = open('tweets.csv', 'a')
+    for tweet in tweepy.Cursor(api.home_timeline).items(10000):
+        if is_english(tweet.text):
+            csv_writer.writerow([tweet.text])
+
+
+def is_english(text):
+        languages_ratios = {}
+        tokens = wordpunct_tokenize(text)
+        words = [word.lower() for word in tokens]
+
+        for language in stopwords.fileids():
+            stopwords_set = set(stopwords.words(language))
+            words_set = set(words)
+            common_elements = words_set.intersection(stopwords_set)
+            languages_ratios[language] = len(common_elements)  # language "score"
+
+        most_rated_language = max(languages_ratios, key=languages_ratios.get)
+        if most_rated_language == 'english':
+            return True
+
+        return False
+
+
